f8a7a6484a40d626749a3029ff8fcf4b75ae6acc
Ilya Fofanov <66513936+Nihaochingiz@users.noreply.github.com>
2020-08-08 19:14:58 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,26 @@
+
+
+import telebot
+
+bot = telebot.TeleBot('1293005579:AAFlpDDME-5daHx7TjR-ExqMM2irXEEOMko')
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Привет, ты написал мне /start', reply_markup=keyboard1)
+
+@bot.message_handler(content_types=['text'])
+def send_text(message):
+    if message.text.lower() == 'привет':
+        bot.send_message(message.chat.id, 'Привет, мой создатель')
+    elif message.text.lower() == 'пока':
+        bot.send_message(message.chat.id, 'Прощай, создатель')
+    elif message.text.lower() == 'я тебя люблю':
+        bot.send_sticker(message.chat.id, 'CAADAgADZgkAAnlc4gmfCor5YbYYRAI')
+
+
+bot.polling()
+
+
+
+
+
